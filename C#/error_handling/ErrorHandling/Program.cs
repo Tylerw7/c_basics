762bd55a7b62a412876bec2c4cafdab6cf5e0ba4
Tylerw7 <t4wettach@gmail.com>
2026-04-23 22:41:46 -0400

    update

---
+++ C#/error_handling/ErrorHandling/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int first = 20;
+        int second = 10;
+
+        try
+        {
+            bool result = first / second == 2;
+            Console.WriteLine(result);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
