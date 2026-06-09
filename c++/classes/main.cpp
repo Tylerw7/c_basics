6d9b81fe8a99985dcfc625298be81ea3a1bb530a
Tylerw7 <t4wettach@gmail.com>
2026-06-08 20:18:26 -0700

    added classes examples

--- c++/classes/main.cpp
+++ c++/classes/main.cpp
@@ -2,13 +2,30 @@
 
 class School
 {
-    public:
+    private:
       std::string name;
       int age;
+      bool enrolled;
+
+    public:
+      School()
+      {
+        this->enrolled = true;
+      }  
+
+      void setName(std::string name)
+      {
+        this->name = name;
+      }
+
+      void setAge(int age)
+      {
+        this->age = age;
+      }
 
       void banner()
       {
-        std::cout << name << " is " << age << "years old.\n";
+        std::cout << name << " is " << age << " years old.\n";
       }
 };
 
@@ -16,9 +33,14 @@ class School
 int main()
 {
     School mine;
-    mine.name = "Tyler";
-    mine.age = 33;
+    mine.setName("Tyler");
+    mine.setAge(33);
 
+    School wife;
+    wife.setName("Precious");
+    wife.setAge(30);
+
+    wife.banner();
     mine.banner();
 
     return 0;
