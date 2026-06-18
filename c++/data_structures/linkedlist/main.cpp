4565c64862b69ef72a4a960ca2c13455c78af286
Tylerw7 <t4wettach@gmail.com>
2026-06-17 19:56:25 -0700

    added cpp linked list

---
+++ c++/data_structures/linkedlist/main.cpp
@@ -0,0 +1,15 @@
+// Linked List
+
+
+// ** NOTE **
+// - Linked lists are a list of values of pointers, each pointer points to a different place in memory
+// - A Vector is different, the memory is contiguous all next to each other.
+
+// BIG O
+//  "ADD To End" ------- O(1)
+//  "Remove from end" -- O(n)
+//  "Add to beginning" - O(1)
+//  "Remove beginning" - O(1)
+//  "Insert" ----------- O(n)
+//  "Remove" ----------- O(n)
+//  "Return value" ----- O(n)
