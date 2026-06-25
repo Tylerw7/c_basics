2de09618df483e5502374de336d93d4b454916dc
Tylerw7 <t4wettach@gmail.com>
2026-06-25 05:15:19 -0700

    added hash tables

---
+++ c++/data_structures/hash_tables/main.cpp
@@ -0,0 +1,15 @@
+
+
+// We will use seperate chaining, not linear probing.
+//We will achieve this by using linked lists.
+
+// ** NOTE **
+// What ever address space number you choose it should
+//  always bw a prime number
+// ** Because if you have a prime number of addresses, 
+//    your key value pairs will be distributed more randomly.
+
+
+
+
+
