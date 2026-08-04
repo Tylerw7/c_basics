d9bbc39aa820a01bbabdccc775b205d88dad59eb
Tylerw7 <t4wettach@gmail.com>
2026-08-04 05:13:21 -0700

    graphs

---
+++ c++/data_structures/graph/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <unordered_set>
+
+// ---------------------------------------
+// GRAPHS
+// ---------------------------------------
+
+
+// nodes/vertices
+// edges/connections
+
+// graphs can be bydirectional
+// edges can be weighted
+
+// Adjacency List
+
+
+
+// Add vertex function
+class Graph {
+    private: 
+      std::unordered_map<std::string, std::unordered_set<std::string>> adjList;
+
+    public:
+
+      bool AddVertex(std::string vertex) {
+        
+      }  
+};
+
