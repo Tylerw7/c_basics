142c824faccbca05dbf2f3d7ac9f2db1769b3e53
Tylerw7 <t4wettach@gmail.com>
2026-08-11 20:43:22 -0700

    added heap data structure

--- c++/data_structures/heap/main.cpp
+++ c++/data_structures/heap/main.cpp
@@ -13,6 +13,8 @@ class Heap {
         return 2 * index + 1;
       }
 
+
+    public:
       int parent(int index) {
         return (index - 1) / 2;
       }
@@ -22,4 +24,39 @@ class Heap {
         heap[index1] = heap[index2];
         heap[index2] = temp;
       }
+
+      void insert(int value) {
+        heap.push_back(value);
+        int current = heap.size() - 1;
+
+        while (current > 0 
+            && heap[current] > heap[parent(current)]) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+      }
+
+      void printHeap() {
+        std::cout << "\n[";
+        for (size_t i = 0; i < heap.size(); i++) {
+            std::cout << heap[i];
+            if (i < heap.size() - 1) std::cout << ", ";
+        }
+        std::cout << "]" << std::endl;
+      }
 };
+
+
+
+int main() {
+
+    Heap* heap1 = new Heap();
+    heap1->insert(5);
+    heap1->insert(50);
+    heap1->insert(40);
+    heap1->insert(90);
+    heap1->printHeap();
+
+
+    return 0;
+}
